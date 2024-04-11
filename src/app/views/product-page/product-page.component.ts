d07235a1450c5699efd84c13186bbdd5a6132e32
debtil <matheus.debtilsouza@gmail.com>
2024-04-11 19:44:18 -0300

    showdown & product-page working

--- src/app/views/product-page/product-page.component.ts
+++ src/app/views/product-page/product-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductStateService } from '../../services/product-state.service';
+import { Product } from '../../models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-page',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './product-page.component.css'
 })
 export class ProductPageComponent {
+  product: Product;
 
+  constructor(private productStateService: ProductStateService, private router: Router, /*private checkoutService: CheckoutService*/){}
+
+  handler: any = null;
+  paymentHandler: any = null;
+
+  ngOnInit(): void{
+    this.productStateService.product$.subscribe((product) =>{
+      this.product = product;
+    })
+  }
 }
