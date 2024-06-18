a11f67ef11478ea834799c93671eda337ea3432b
debtil <matheus.debtilsouza@gmail.com>
2024-06-18 18:54:52 -0300

    size image on showdown & fontsize on navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
     extend: {colors: {
       primary: {"50":"#f0f9ff","100":"#e0f2fe","200":"#bae6fd","300":"#7dd3fc","400":"#38bdf8","500":"#0ea5e9","600":"#0284c7","700":"#0369a1","800":"#075985","900":"#0c4a6e","950":"#082f49"}
     },
+    fontSize: {
+      'md': '1.125rem', // 18px
+    },
     gridTemplateRows: {
       '[auto,auto,1fr]': 'auto auto 1fr',
     },
